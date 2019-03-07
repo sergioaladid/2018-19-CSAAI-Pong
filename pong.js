943509fa7a35b41b189c0b07bf58020791a707e6
Sergio Aladid De Andres <saladid@beta17.aulas.gsyc.urjc.es>
2019-03-07 10:38:41 +0100

    Primera animación

--- pong.js
+++ pong.js
@@ -35,14 +35,70 @@ function main()
   ctx.fillStyle = 'white'
   ctx.fillText("0", 340, 70);
 
-  //-- Bola
-  ctx.beginPath();
-  ctx.arc(150, 200, 3, 0, 2 * Math.PI);
-  ctx.strokeStyle = 'white';
-  ctx.lineWidth = 3;
-  ctx.stroke()
-  ctx.fillStyle = 'white';
-  ctx.fill()
+  var bola = {
 
+    x_ini: 50,
+    y_ini: 50,
+
+    vx: 4,
+    vy: 1,
+
+    x: 0,
+    y: 0,
+
+    ctx: null,
+
+    init: function(ctx) {
+      this.ctx = ctx;
+      this.reset();
+    },
+    draw: function () {
+
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, 3, 0, 2 * Math.PI);
+      ctx.strokeStyle = 'white';
+      ctx.lineWidth = 3;
+      ctx.stroke()
+      ctx.fillStyle = 'white';
+      ctx.fill()
+    },
+    update: function () {
+      this.x += this.vx;
+      this.y += this.vy;
+    },
+    reset: function() {
+      this.x = this.x_ini;
+      this.y = this.y_ini;
+    }
+  }
+
+  bola.init(ctx)
+  bola.draw()
+
+  var timer = null;
+  var sacar = document.getElementById('sacar')
+
+  sacar.onclick = () => {
+
+    if (!timer) {
+
+      timer = setInterval(()=>{
+
+        bola.update();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        bola.draw();
+
+        if (bola.x > canvas.width) {
+
+          clearInterval(timer)
+          timer = null;
+
+          bola.reset();
+
+          bola.draw();
+        }
+      },20);
+    }
+  }
 
 }
